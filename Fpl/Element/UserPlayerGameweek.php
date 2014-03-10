891da078d8b1268f511fc5f496b544befc73c994
Jared Mellentine <jared@mlntn.com>
2014-03-09 17:08:11 -0700

    User gameweek players

--- Fpl/Element/UserPlayerGameweek.php
+++ Fpl/Element/UserPlayerGameweek.php
@@ -8,13 +8,20 @@ class UserPlayerGameweek extends PlayerGameweek {
    * @var boolean
    */
   public $is_captain;
+
   /**
    * @var boolean
    */
   public $is_vice_captain;
+
   /**
    * @var boolean
    */
   public $is_starter;
+
+  /**
+   * @var integer
+   */
+  public $substitute_order = 0;
 
 }
